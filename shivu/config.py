2f229624b22ffb40218bd22482ce835a272b53a4
LUCAS-9978 <kumararvind06757@gmail.com>
2025-05-12 00:56:45 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -8,14 +8,14 @@ class Config(object):
 
     TOKEN = "8160950383:AAE4ddE0nG9R2xCbyG_u1a7lYBXngFdyRlc"
     mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
-    PARTNER = "7361967332", "7795212861", "5758240622"
+    PARTNER = ""
     PHOTO_URL = ["https://files.catbox.moe/7vr2im.jpg", "https://files.catbox.moe/7vr2im.jpg"]
-    SUPPORT_CHAT = "https://t.me/hwkwjieie"
-    UPDATE_CHAT = "https://t.me/DBZ_COMMUNITY_2"
+    SUPPORT_CHAT = "https://t.me/XYZ_SUPPORT_TM"
+    UPDATE_CHAT = "https://t.me/XYZ_UPDATE_TM"
     BOT_USERNAME = "@XYZ_WAIFU_BOT"
-    CHARA_CHANNEL_ID = "-1002621413939"
-    api_id = 23287799
-    api_hash = "9f4f17dae2181ee22c275b9b40a3c907"
+    CHARA_CHANNEL_ID = "-1002296333139"
+    api_id = 20737348
+    api_hash = "c5a1aa4d0aa8a25c39b44eb456118e12"
 
 
 class Production(Config):
