e30536da27f741f7990b3ec15b0188bba8cc246e
LUCAS-9978 <kumararvind06757@gmail.com>
2025-05-12 00:36:47 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,13 +6,13 @@ class Config(object):
     sudo_users = ""
     GROUP_ID = -1002296333139
 
-    TOKEN = "7369319572:AAEqECI_jCRUYzXIPk2KmoUyXJs4vDYhA2w"
+    TOKEN = "8160950383:AAE4ddE0nG9R2xCbyG_u1a7lYBXngFdyRlc"
     mongo_url = "mongodb+srv://naruto:hinatababy@cluster0.rqyiyzx.mongodb.net/"
     PARTNER = "7361967332", "7795212861", "5758240622"
     PHOTO_URL = ["https://files.catbox.moe/7vr2im.jpg", "https://files.catbox.moe/7vr2im.jpg"]
     SUPPORT_CHAT = "https://t.me/hwkwjieie"
     UPDATE_CHAT = "https://t.me/DBZ_COMMUNITY_2"
-    BOT_USERNAME = "@Dbz_waifubot"
+    BOT_USERNAME = "@XYZ_WAIFU_BOT"
     CHARA_CHANNEL_ID = "-1002621413939"
     api_id = 23287799
     api_hash = "9f4f17dae2181ee22c275b9b40a3c907"
